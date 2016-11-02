229912c515743d5b901fe549f70e6fd8171d1729
Vladutu <vladutu_georgian_4d@yahoo.com>
2016-11-02 12:41:09 +0200

    -created angular 2 app without angular-cli

---
+++ app/app.module.ts
@@ -0,0 +1,24 @@
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {AppComponent} from "./app.component";
+import {DropdownDirective} from "./dropdown.directive";
+import {ProfessorNavbarComponent} from "./professor-navbar/professor-navbar.component";
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        DropdownDirective,
+        ProfessorNavbarComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ app/professor-navbar/professor-navbar.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+    moduleId: module.id,
+    selector: 'wad-professor-navbar',
+    templateUrl: 'professor-navbar.component.html',
+    styleUrls: ['professor-navbar.component.css']
+})
+export class ProfessorNavbarComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
