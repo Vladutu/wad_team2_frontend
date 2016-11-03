2e1c0463801a37e361c9e94647c1acda5c5fc97a
Vladutu <vladutu_georgian_4d@yahoo.com>
2016-11-03 16:44:38 +0200

    - added routes to secretary professor list, student list and subgroup list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 import {AppComponent} from "./app.component";
-import { LoginComponent } from './login/login.component';
-import { SecretaryComponent } from './secretary/secretary.component';
-import { DropdownDirective } from './dropdown.directive';
-import { SubgroupListComponent } from './secretary/subgroup-list/subgroup-list.component';
+import {LoginComponent} from "./login/login.component";
+import {SecretaryComponent} from "./secretary/secretary.component";
+import {DropdownDirective} from "./secretary/directive/dropdown.directive";
+import {SubgroupListComponent} from "./secretary/subgroup-list/subgroup-list.component";
+import {ProfessorListComponent} from "./secretary/professor-list/professor-list.component";
+import {StudentListComponent} from "./secretary/student-list/student-list.component";
+import {routing} from "./app.routes";
 
 @NgModule({
   declarations: [
@@ -15,11 +18,14 @@ import { SubgroupListComponent } from './secretary/subgroup-list/subgroup-list.c
     SecretaryComponent,
     DropdownDirective,
     SubgroupListComponent,
+    ProfessorListComponent,
+    StudentListComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
