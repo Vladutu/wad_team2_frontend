2c2db0ea3dada36fc63e4e711ff663936f787e77
Vladutu <vladutu_georgian_4d@yahoo.com>
2016-11-09 15:50:11 +0200

    - added kendo table - set navbar on all page - changed overlay spinning icon

--- src/app/secretary/subgroup-list/subgroup-list.component.ts
+++ src/app/secretary/subgroup-list/subgroup-list.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit, ViewChild} from "@angular/core";
 import {ModalComponent} from "ng2-bs3-modal/components/modal";
+import {SortDescriptor, orderBy} from "@progress/kendo-data-query";
+import {GridDataResult} from "@progress/kendo-angular-grid";
 
 @Component({
   selector: 'wad-subgroup-list',
@@ -13,10 +15,48 @@ export class SubgroupListComponent implements OnInit {
 
   private requesting: boolean = false;
 
+  private subgroups: any[] = [];
+
+  private sort: SortDescriptor[] = [];
+
+  private gridView: GridDataResult;
+
   constructor() {
   }
 
   ngOnInit() {
+    this.subgroups = [
+      {
+        id: 154,
+        name: "CEN4.S1"
+      },
+      {
+        id: 2362,
+        name: "CEN4.S2"
+      },
+      {
+        id: 3573,
+        name: "CEN3.S1"
+      },
+      {
+        id: 4712,
+        name: "CEN3.S2"
+      },
+      {
+        id: 5751,
+        name: "CEN2.A"
+      },
+      {
+        id: 631,
+        name: "CEN2.B"
+      },
+      {
+        id: 7731,
+        name: "CEN2.C"
+      }
+    ];
+
+    this.loadSubgroups();
   }
 
   onSave(): void {
@@ -27,5 +67,22 @@ export class SubgroupListComponent implements OnInit {
       this.formModal.close();
     }, 2000);
   }
+
+  private sortChange(sort: SortDescriptor[]): void {
+    this.sort = sort;
+    this.loadSubgroups();
+  }
+
+  private loadSubgroups(): void {
+    this.gridView = {
+      data: orderBy(this.subgroups, this.sort),
+      total: this.subgroups.length
+    };
+  }
+
+  private onDelete(subgroup: any): void {
+    console.log(subgroup);
+  }
+
 
 }
